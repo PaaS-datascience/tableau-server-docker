8409499786d32dcf6dde1dd6add48e95d45450ca
Fabien <fabien.antoine@m4x.org>
2018-04-11 15:49:56 +0000

    dirty things to be cleaned later

---
+++ config/tableau-start.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Exit on first error:w
+set -e
+
+source /etc/profile.d/tableau_server.sh
+
+LOGFILE=/var/log/tableau_docker.log
+RETAIN_NUM_LINES=10000
+
+function logsetup {  
+    TMP=$(tail -n $RETAIN_NUM_LINES $LOGFILE 2>/dev/null) && echo "${TMP}" > $LOGFILE
+    exec > >(tee -a $LOGFILE)
+    exec 2>&1
+}
+
+function log {  
+    echo "[$(date --rfc-3339=seconds)]: $*"
+}
+
+su tsm -c "sudo /opt/tableau/tableau_server/packages/customer-bin.${TABLEAU_SERVER_DATA_DIR_VERSION}/tsm start" 2>&1 1>> /var/log/tableau_docker.log
+
+log stopped tsm done
